349ddfe88e10505afc92fae6fc3b2b404cae4915
diego-ac22 <da.acostac1@uniandes.edu.co>
2024-11-10 16:16:23 -0500

    cambios iniciales

---
+++ data.ts
@@ -0,0 +1,7 @@
+export class Serie
+{
+    constructor(public id: number,public nombres: string, public canales: string, public temporadas: number, public descripcion: string, public link:string, public imagen:string)
+    {
+
+    }
+}
